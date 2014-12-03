a3b00896e8797ee41917d44aa4215f9afd7e345a
Fredrik Frodlund <fredrik.frodlund@jayway.com>
2014-12-03 12:49:36 +0100

    Add swedish translation

---
+++ locale/sv.php
@@ -0,0 +1,18 @@
+<?php if (!defined('APPLICATION')) exit;
+
+$Definition['timeago.prefixAgo']     = null;
+$Definition['timeago.prefixFromNow'] = null;
+$Definition['timeago.suffixAgo']     = 'sedan';
+$Definition['timeago.suffixFromNow'] = 'från nu';
+$Definition['timeago.seconds']       = 'mindre än en minut';
+$Definition['timeago.minute']        = 'ungefär en minut';
+$Definition['timeago.minutes']       = '%d minuter';
+$Definition['timeago.hour']          = 'ungefär en timme';
+$Definition['timeago.hours']         = 'ungefär %d timmar';
+$Definition['timeago.day']           = 'en dag';
+$Definition['timeago.days']          = '%d dagar';
+$Definition['timeago.month']         = 'ungefär en månad';
+$Definition['timeago.months']        = '%d månader';
+$Definition['timeago.year']          = 'ungefär ett år';
+$Definition['timeago.years']         = '%d år';
+$Definition['timeago.wordSeparator'] = ' ';
